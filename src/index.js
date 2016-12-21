8f18488389cff1d4fe9943489248f832da422ae3
Nick Schwab <git@nickschwab.com>
2016-12-21 17:30:05 -0500

    - added fallback to Yahoo stock lookup - added VoiceLabs analytics - added custom override rules for companies Alexa has difficulty interpreting and/or companies difficult to find the ticker symbol for

--- src/index.js
+++ src/index.js
@@ -11,9 +11,13 @@ var APP_ID = "";
 var APP_NAME = "Opening Bell";
 
 var MOD_BASE_URL = "http://dev.markitondemand.com/MODApis/Api/v2";
+var YQL_BASE_URL = "http://query.yahooapis.com/v1/public/yql";
+var VI_APP_TOKEN = '';
 
 var http = require('http');
 var async = require('async');
+var overrides = require('./overrides');
+var VoiceInsights = require('voice-insights-sdk');
 require('string_score');
 
 /**
@@ -30,6 +34,7 @@ MyApp.prototype = Object.create(AlexaSkill.prototype);
 MyApp.prototype.constructor = MyApp;
 
 MyApp.prototype.eventHandlers.onSessionStarted = function (sessionStartedRequest, session) {
+    VoiceInsights.initialize(session, VI_APP_TOKEN);
     console.log("MyApp onSessionStarted requestId: " + sessionStartedRequest.requestId
         + ", sessionId: " + session.sessionId);
     // any initialization logic goes here
@@ -61,14 +66,17 @@ MyApp.prototype.intentHandlers = {
     },
 
     "AMAZON.HelpIntent": function (intent, session, response) {
+        VoiceInsights.track(intent.name);
         response.ask("I can tell you the stock price of any publicly traded company such as Apple, Tesla, Microsoft, and many more. Which would you like?");
     },
 
     "AMAZON.StopIntent": function (intent, session, response) {
+        VoiceInsights.track(intent.name);
         response.tell("O.K.");
     },
 
     "AMAZON.CancelIntent": function (intent, session, response) {
+        VoiceInsights.track(intent.name);
         response.tell("O.K.");
     }
 };
@@ -82,65 +90,128 @@ function handleTickerRequest(company_name, response) {
 function getTickerText(company_name, callback){
     async.waterfall([
         function(callback){
-            // look up the company's ticker
-            http.get(MOD_BASE_URL + "/Lookup/json?input=" + company_name, function(res){
-                var body = "";
-        
-                res.on('data', function (chunk) {
-                    body += chunk;
-                });
-                
-                res.on('end', function () {
-                    try{
-                        var result = JSON.parse(body);
-                        
-                        if(result.length){
-                            callback(null, result);
-                        }else{
-                            callback("No matching companies", null);
-                        }
-                    }catch(e){
-                        callback(e, null);
-                    }
-                });
-            }).on('error', function (e) {
-                console.log("Got error: ", e);
-                callback(e, null);
-            });
-        },
-        function(companies, callback){
-            if(companies.length == 1){
-                // single search result, use it regardless of confidence match
-                callback(null, companies[0].Symbol);
-                return;
-            }
-            
-            // sort array of company matches based on strongest name match, descending
-            async.sortBy(companies, function(company, callback){
-                console.log(company.Name.toLowerCase() + ": " + company_name.toLowerCase().score(company.Name.toLowerCase(), 1));
-                callback(null, company_name.toLowerCase().score(company.Name.toLowerCase(), 1)*-1);
-            }, function(err, sorted_companies){
-                // use the ticker symbol with the strongest name match confidence score
-                callback(err, sorted_companies[0].Symbol);
+            // check for a override before doing an external API ticker Lookup
+            async.detect(overrides, function(item, callback){
+                async.detect(item.aliases, function(alias, callback){
+                    callback(null, alias == company_name.toLowerCase());
+                }, callback);
+            }, function(err, match){
+                callback(err, match ? match.ticker : null);
             });
         },
         function(symbol, callback){
+            console.log("override symbol: " + symbol);
+            if(symbol){
+                callback(null, symbol);
+                return;
+            }
+
+            async.waterfall([
+                function(callback){
+                    // look up the company's ticker
+                    http.get(MOD_BASE_URL + "/Lookup/json?input=" + company_name, function(res){
+                        var body = "";
+
+                        res.on('data', function (chunk) {
+                            body += chunk;
+                        });
+
+                        res.on('end', function () {
+                            try{
+                                var result = JSON.parse(body);
+
+                                if(result.length){
+                                    callback(null, result);
+                                }else{
+                                    callback("No matching companies", null);
+                                }
+                            }catch(e){
+                                callback(e, null);
+                            }
+                        });
+                    }).on('error', function (e) {
+                        console.log("Got error: ", e);
+                        callback(e, null);
+                    });
+                },
+                function(companies, callback){
+                    if(companies.length == 1){
+                        // single search result, use it regardless of confidence match
+                        callback(null, companies[0].Symbol);
+                        return;
+                    }
+
+                    // sort array of company matches based on strongest name match, descending
+                    async.sortBy(companies, function(company, callback){
+                        console.log(company.Name.toLowerCase() + ": " + company_name.toLowerCase().score(company.Name.toLowerCase(), 1));
+                        callback(null, company_name.toLowerCase().score(company.Name.toLowerCase(), 1)*-1);
+                    }, function(err, sorted_companies){
+                        // use the ticker symbol with the strongest name match confidence score
+                        callback(err, sorted_companies[0].Symbol);
+                    });
+                }
+            ], callback);
+        },
+        function(symbol, callback){
+            async.parallel({
+                "yql": function(callback){
+                    getTickerTextFromYql(symbol, function(err, text){
+                        if(err){
+                            console.log(err);
+                        }
+                        callback(null, text);
+                    });
+                },
+                "mod": function(callback){
+                    getTickerTextFromMod(symbol, function(err, text){
+                        if(err){
+                            console.log(err);
+                        }
+                        callback(null, text);
+                    });
+                }
+            }, function(err, result){
+                var text = result.yql || result.mod || null;
+                callback(null, text);
+            });
+        }
+    ], function(err, result){
+        if(err){
+            console.log(err);
+            console.log(result);
+        }
+        callback(null, err || !result ? "I can't access that company's data right now, please try again later." : result);
+        VoiceInsights.track("GetTickerIntent", company_name, (err || !result ? "error" : "success"));
+    });
+}
+
+// Create the handler that responds to the Alexa Request.
+exports.handler = function (event, context) {
+    // Create an instance of the MyApp skill.
+    var skill = new MyApp();
+    skill.execute(event, context);
+};
+
+
+function getTickerTextFromMod(symbol, callback){
+    async.waterfall([
+        function(callback){
             // get the stock price
             http.get(MOD_BASE_URL + "/Quote/json?symbol=" + symbol, function(res) {
                 var body = "";
-        
+
                 res.on('data', function (chunk) {
                     body += chunk;
                 });
-        
+
                 res.on('end', function () {
                     try{
                         var result = JSON.parse(body);
-                        
+
                         if(result.Name){
                             callback(null, result)
                         }else{
-                            callback("No price record", null);
+                            callback("No MOD price record for " + symbol, null);
                         }
                     }catch(e){
                         callback(e, null);
@@ -167,19 +238,55 @@ function getTickerText(company_name, callback){
                 callback("error", null);
             }
         }
-    ], function(err, result){
-        if(err){
-            console.log(err);
-            console.log(result);
-        }
-        callback(null, err ? "Sorry, I couldn't find the stock you're looking for." : result);
-    });
+    ], callback);
 }
 
-// Create the handler that responds to the Alexa Request.
-exports.handler = function (event, context) {
-    // Create an instance of the MyApp skill.
-    var skill = new MyApp();
-    skill.execute(event, context);
-};
 
+function getTickerTextFromYql(symbol, callback){
+    async.waterfall([
+        function(callback){
+            // get the stock price
+            http.get(YQL_BASE_URL + '?q=select%20*%20from%20yahoo.finance.quotes%20where%20symbol%20in%20("' + symbol + '")&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys&callback=', function(res) {
+                var body = "";
+
+                res.on('data', function (chunk) {
+                    body += chunk;
+                });
+
+                res.on('end', function () {
+                    try{
+                        var result = JSON.parse(body);
+
+                        if(result.query && result.query.results && result.query.results.quote){
+                            callback(null, result.query.results.quote)
+                        }else{
+                            callback("No YQL price record for " + symbol, null);
+                        }
+                    }catch(e){
+                        callback(e, null);
+                    }
+                });
+            }).on('error', function (e) {
+                console.log("Got error: ", e);
+                callback(e, null);
+            });
+        },
+        function(company, callback){
+            // convert it into a readable string
+            if(company){
+                var text = company.Name + " is trading at " + (Math.round((Math.abs(company.LastTradePriceOnly) + 0.00001) * 100) / 100) + ". ";
+                company.ChangeinPercent = parseFloat(company.ChangeinPercent);
+                if(company.ChangeinPercent > 0){
+                    text += "Up " + Math.abs(company.ChangeinPercent) + "%.";
+                }else if(company.ChangeinPercent < 0){
+                    text += "Down " + Math.abs(company.ChangeinPercent) + "%.";
+                }else{
+                    text += "No change.";
+                }
+                callback(null, text);
+            }else{
+                callback("error", null);
+            }
+        }
+    ], callback);
+}
---
+++ src/overrides.js
@@ -0,0 +1,160 @@
+module.exports = [{
+    "search_name": "capital one",
+    "ticker": "COF",
+    "aliases": [
+        "capital 1"
+    ]
+}, {
+    "search_name": "dow jones industrial average",
+    "ticker": "DIA",
+    "aliases": [
+        "the dow",
+        "dow jones",
+        "down jones",
+        "djia",
+        "dji",
+        "dia"
+    ]
+}, {
+    "search_name": "delta air line",
+    "ticker": "DAL",
+    "aliases": [
+        "delta",
+        "delta airline",
+        "delta airlines"
+    ]
+}, {
+    "search_name": "google",
+    "ticker": "GOOGL",
+    "aliases": [
+        "google dot com"
+    ]
+}, {
+    "search_name": "ford motor co",
+    "ticker": "F",
+    "aliases": [
+        "ford",
+        "ford motor",
+        "ford motors",
+        "ford motor company",
+        "ford motors company"
+    ]
+}, {
+    "search_name": "procter & gamble co",
+    "ticker": "PG",
+    "aliases": [
+        "procter and gamble",
+        "procter and gamble co",
+        "procter and gamble company"
+    ]
+}, {
+    "search_name": "hershey co",
+    "ticker": "HSY",
+    "aliases": [
+        "hershey",
+        "hersheys",
+        "hershey's",
+        "the hershey company"
+    ]
+}, {
+    "search_name": "synaptics",
+    "ticker": "SYNA",
+    "aliases": [
+        "synaptic",
+        "synaptics",
+        "synoptic",
+        "synoptics"
+    ]
+}, {
+    "search_name": "s & p 500",
+    "ticker": "SPX",
+    "aliases": [
+        "the s and p 500",
+        "s and p 500",
+        "s p 500"
+    ]
+}, {
+    "search_name": "gilead",
+    "ticker": "GILD",
+    "aliases": [
+        "gilead",
+        "gilead sciences",
+        "gilead sciences inc",
+        "gilead sciences incorporated"
+    ]
+}, {
+    "search_name": "transocean",
+    "ticker": "RIG",
+    "aliases": [
+        "transocean",
+        "trans ocean",
+        "transocean limited"
+    ]
+}, {
+    "search_name": "transocean partners",
+    "ticker": "RIGP",
+    "aliases": [
+        "transocean partners",
+        "trans ocean partners"
+    ]
+}, {
+    "search_name": "alibaba",
+    "ticker": "BABA",
+    "aliases": [
+        "alibaba",
+        "baba"
+    ]
+}, {
+    "search_name": "t",
+    "ticker": "T",
+    "aliases": [
+        "8 tnt",
+        "at and t",
+        "a t and t",
+        "att",
+        "a t t"
+    ]
+}, {
+    "search_name": "novartis",
+    "ticker": "NVS",
+    "aliases": [
+        "novartis",
+        "novartis pharmaceutical",
+        "novartis pharmaceuticals"
+    ]
+}, {
+    "search_name": "nvidia",
+    "ticker": "NVDA",
+    "aliases": [
+        "nvidia",
+        "nvidia corp",
+        "nvidia corporation"
+    ]
+}, {
+    "search_name": "conformis",
+    "ticker": "CFMS",
+    "aliases": [
+        "confor mis",
+        "conformis",
+        "trans ocean",
+        "transocean limited"
+    ]
+}, {
+    "search_name": "general electric",
+    "ticker": "GE",
+    "aliases": [
+        "ge",
+        "general electric",
+        "general electric co",
+        "general electric company"
+    ]
+}, {
+    "search_name": "freeport-mcmoran",
+    "ticker": "FCX",
+    "aliases": [
+        "freeport mc moran",
+        "free port mc moran",
+        "freeport mcmoran",
+        "freeport mc moran inc"
+    ]
+}];
